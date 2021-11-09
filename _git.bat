a170714ebebee392f84c089a904d502621efd5da
shenweiyan <ishenweiyan@foxmail.com>
2021-11-09 14:53:52 +0800

    update v2.8.5

---
+++ _git.bat
@@ -0,0 +1,5 @@
+git status
+git add -A
+git commit -m "update the code can't scroll for iphone "
+git push
+pause
---
+++ _git.bat.bak
@@ -0,0 +1,5 @@
+git status
+git add -A
+git commit -m "first version"
+git push
+pause
